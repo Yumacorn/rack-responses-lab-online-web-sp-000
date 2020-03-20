032cb040144a1d40745b931b9765af761eb1e5c8
Ryan Mui <reachryanmui@gmail.com>
2020-03-20 03:17:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ require 'rack'
 class Application
   def call(env)
     resp = Rack::Response.new
-    if Time.hour < 12
+    if hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon"
