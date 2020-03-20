b0cb83d55045be6433b35965b8c9fa93221be926
Ryan Mui <reachryanmui@gmail.com>
2020-03-20 03:15:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ require 'rack'
 class Application
   def call(env)
     resp = Rack::Response.new
-    raise Time.now.inspect.class
+    raise Time.now.classinspect
     if
       resp.write "Good Morning!"
     else
